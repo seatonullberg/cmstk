8b34f4509fe8583e7a6491e0c14482134803a6a2
seatonullberg <sullberg@ufl.edu>
2019-04-14 18:39:16 -0400

    added loss functions

--- cmstk/optimization/loss.py
+++ cmstk/optimization/loss.py
@@ -1,16 +1,65 @@
+import numpy as np
+
+# TODO: double check which axis these operation should occur on
+
+class LossFunction(object):
+    """Representation of generalized loss functions with single initialization and validation.
+    
+    Args:
+        evaluation_function (function): The underlying loss function.
+    """
+
+    def __init__(self, evaluation_function):
+        if not callable(evaluation_function):
+            raise TypeError("`evaluation_function` must be callable")
+        self._evaluation_function = evaluation_function
+
+    def loss(self, targets, actuals):
+        """Calculates the loss as specified by self._evaluation_function.
+        
+        Args:
+            targets (np.ndarray): Array of target values.
+            actuals (np.ndarray): Array of actual values.
+
+        Returns:
+            np.ndarray
+        """
+        if type(targets) is not np.ndarray:
+            raise TypeError("`targets` must be of type np.ndarray")
+        if type(actuals) is not np.ndarray:
+            raise TypeError("`actuals` must be of type np.ndarray")
+        return self._evaluation_function(targets, actuals)
 
 
-def mean_square_error():
-    raise NotImplementedError
+class LogCoshError(LossFunction):
+    """Implementation of the log cosh loss function."""
+
+    def __init__(self):
+        super().__init__(self._log_cosh_error)
+
+    @staticmethod
+    def _log_cosh_error(targets, actuals):
+        loss = np.log(np.cosh(actuals - targets))
+        return np.sum(loss)
 
 
-def mean_absolute_error():
-    raise NotImplementedError
+class MeanAbsoluteError(LossFunction):
+    """Implementation of the mean absolute error loss function."""
+
+    def __init__(self):
+        super().__init__(self._mean_absolute_error)
+
+    @staticmethod
+    def _mean_absolute_error(targets, actuals):
+        return (np.absolute(targets - actuals)).mean(axis=0)
 
 
-def smooth_mean_absolute_error():
-    raise NotImplementedError
+class MeanSquareError(LossFunction):
+    """Implementation of the mean square error loss function."""
 
+    def __init__(self):
+        super().__init__(self._mean_square_error)
 
-def log_cosh_error():
-    raise NotImplementedError
+    @staticmethod
+    def _mean_square_error(targets, actuals):
+        return (np.square(targets - actuals)).mean(axis=0)
---
+++ cmstk/optimization/test_loss.py
@@ -0,0 +1,24 @@
+import numpy as np
+from cmstk.optimization.loss import MeanAbsoluteError, MeanSquareError, LogCoshError
+
+
+def test_mean_absolute_error():
+    targets = np.array([1.5, 1.5, 1.5])
+    actuals = np.array([1.0, 1.0, 1.0])
+    mae = MeanAbsoluteError()
+    loss = mae.loss(targets, actuals)
+    assert loss == 0.5
+
+def test_mean_square_error():
+    targets = np.array([1.5, 1.5, 1.5])
+    actuals = np.array([1.0, 1.0, 1.0])
+    mse = MeanSquareError()
+    loss = mse.loss(targets, actuals)
+    assert loss == 0.25
+
+def test_log_cosh_error():
+    targets = np.array([1.5, 1.5, 1.5])
+    actuals = np.array([1.0, 1.0, 1.0])
+    lce = LogCoshError()
+    loss = lce.loss(targets, actuals)
+    assert loss == 0.36034352087483235
