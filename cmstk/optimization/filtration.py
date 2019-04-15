627dc7e886291d1bbb0d6a16082467470d9a5e86
seatonullberg <sullberg@ufl.edu>
2019-04-14 22:18:12 -0400

    added filter sets

--- cmstk/optimization/filtration.py
+++ cmstk/optimization/filtration.py
@@ -17,41 +17,51 @@ class BaseFilter(object):
 
 
 class ParetoFilter(BaseFilter):
-    """Implementation of a Pareto efficiency filter."""
+    """Implementation of a Pareto efficiency filter.
+    
+    Args:
+        arr (numpy.ndarray): Array of costs.
+    """
 
-    def __init__(self):
+    def __init__(self, arr):
         super().__init__(self)
+        if type(arr) is not np.ndarray:
+            raise TypeError("`arr` must be of type np.ndarray")
+        self._arr = arr
 
-    def filter(self, arr):
+    def filter(self):
         """Returns a mask of Pareto efficient points.
-        
-        Args:
-            arr (numpy.ndarray): Array of costs.
 
         Returns:
             numpy.ndarray
         """
-        is_efficient = np.ones(arr.shape[0], dtype=bool)
+        is_efficient = np.ones(self._arr.shape[0], dtype=bool)
         for i, cost in enumerate(arr):
             if is_efficient[i]:
-                is_efficient[is_efficient] = np.any(arr[is_efficient] < cost, axis=1)  # Keep any point with a lower cost
+                is_efficient[is_efficient] = np.any(self._arr[is_efficient] < cost, axis=1)  # Keep any point with a lower cost
                 is_efficient[i] = True  # And keep self
         return is_efficient
 
 
 class PercentileFilter(BaseFilter):
-    """Implementation of a percentile filter."""
+    """Implementation of a percentile filter.
+    
+    Args:
+        arr (numpy.ndarray): Array of costs.
+        percentile (float): The percentile rank below which observations are excluded.
+    """
 
-    def __init__(self):
+    def __init__(self, arr, percentile):
         super().__init__(self)
+        if type(arr) is not np.ndarray:
+            raise TypeError("`arr` must be of type numpy.ndarray")
+        if type(percentile) is not float:
+            raise TypeError("`percentile` must be of type float")
+        self._arr = arr
+        self._percentile = percentile
 
-    def filter(self, arr, percentile):
+    def filter(self):
         """Returns a mask of points at or above the given percentile.
-        
-        Args:
-            arr (numpy.ndarray): Array of costs.
-            percentile (float): The percentile rank below which observations are excluded.
-            - a value of 0.95 would return a mask which exposes the top 5% of performance space.
         
         Returns:
             numpy.ndarray
@@ -62,17 +72,25 @@ class PercentileFilter(BaseFilter):
 # TODO: may want to create a constraint object rather than use a general dictionary?
 # - qoi object keys with max and min value properties
 class ConstraintFilter(BaseFilter):
-    """Implementation of a quantity of interest based constraint filter."""
+    """Implementation of a quantity of interest based constraint filter.
+    
+    Args:
+        arr (numpy.ndarray): Array of costs.
+        constraints (dict): Dictionary of constraints by qoi
+    """
 
-    def __init__(self):
+    def __init__(self, arr, constraints):
         super().__init__(self)
+        if type(arr) is not np.ndarray:
+            raise TypeError("`arr` must be of type numpy.ndarray")
+        if type(constraints) is not dict:
+            raise TypeError("`constraints` must be of type dict")
+        self._arr = arr
+        self._constraints = constraints
 
-    def filter(self, arr, constraints):
+
+    def filter(self):
         """Returns a mask of points which satisfy the constraints.
-        
-        Args:
-            arr (numpy.ndarray): Array of costs.
-            constraints (dict): Dictionary of constraints by quantity of interest. 
 
         Returns:
             numpy.ndarray
@@ -84,35 +102,71 @@ class BaseFilterSet(object):
     """Representation of a generalized combination of filter objects.
     
     Args:
-        filters (list of BaseFilter instances): The filters to be combined.
+        obj (instance of BaseFilterSet): A BaseFilterSet instance used to ensure it has an `apply` method.
     """
 
-    def __init__(self, filters):
-        if type(filters) is not list:
-            raise TypeError("`filters` must be of type list")
-        for f in filters:
-            if not isinstance(f, BaseFilter):
-                raise TypeError("all members of `filter` must be instances of type BaseFilter")
-        self._filters = filters
+    def __init__(self, obj):
+        obj_methods = [method_name for method_name in dir(object) if callable(getattr(obj, method_name))]
+        if "apply" not in obj_methods:
+            raise ValueError("`obj` must implement a method called `apply`")
+        self._filters = []
+
+    def add_filter(self, f):
+        if not isinstance(f, BaseFilter):
+            raise TypeError("`f` must be an instance of type BaseFilter")
+        self._filters.append(f)
 
 
 class IntersectionalFilterSet(BaseFilterSet):
-    """Implementation of a filter set which applies the intersection of all filter masks.
-    
-    Args:
-        filters (list of BaseFilter instances): The filters to be combined.
-    """
+    """Implementation of a filter set which applies the intersection of all filter masks simultaneously."""
 
-    def __init__(self, filters)
-        super().__init__(filters)
+    def __init__(self)
+        super().__init__(self)
+
+    def apply(self, arr):
+        """Returns the filtered results.
+        
+        Args:
+            arr (numpy.ndarray): Array of values to be filtered.
+
+        Returns:
+            numpy.ndarray
+            - The same array, filtered.
+        """
+        masks = [f.filter() for f in self._filters]
+        if len(masks) == 0:
+            return arr  # return the unfiltered array
+        if len(masks) == 1:
+            return arr[masks[0]]
+        # apply intersection of all masks
+        current_mask = masks[0]
+        for m in masks[1:]:
+            current_mask = np.logical_and(current_mask, m)
+        return arr[current_mask]
 
 
 class SequentialFilterSet(BaseFilterSet):
-    """Implementation of a filter set which applies its filters in the sequence of their addition.
+    """Implementation of a filter set which applies all filter masks in the sequence of their addition."""
     
-    Args:
-        filters (list of BaseFilter instances): The filters to be combined.
-    """
-    
-    def __init__(self, filters):
-        super().__init__(filters)
+    def __init__(self):
+        super().__init__(self)
+
+    def apply(self, arr):
+        """Returns the filtered results.
+        
+        Args:
+            arr (numpy.ndarray): Array of values to be filtered.
+
+        Returns:
+            numpy.ndarray
+            - The same array, filtered.
+        """
+        masks = [f.filter() for f in self._filters]
+        if len(masks) == 0:
+            return arr  # return the unfiltered array
+        if len(masks) == 1:
+            return arr[masks[0]]
+        # apply masks in sequence
+        for m in masks:
+            arr = arr[m]
+        return arr
