5ff83f1f5bbbd1dfaeed2a8ba0e45b71ba8e2912
seatonullberg <seatonullberg@gmail.com>
2019-04-18 18:27:37 -0400

    more lammps wrapper work

--- cmstk/lammps/wrapper.py
+++ cmstk/lammps/wrapper.py
@@ -45,29 +45,35 @@ class LAMMPS(object):
 
     @property
     def has_gzip_support(self):
-        # TODO
-        pass
+        """True if LAMMPS compiled with C++ gzip support."""
+        return self._libc.lammps_config_has_gzip_support() != 0
 
     @property
     def has_png_support(self):
-        # TODO
-        pass
+        """True if LAMMPS compiled with C++ png support."""
+        return self._libc.lammps_config_has_png_support() != 0
 
     @property
     def has_jpeg_support(self):
-        # TODO
-        pass
+        """True if LAMMPS compiled with C++ jpeg support."""
+        return self._libc.lammps_config_has_jpeg_support() != 0
 
     @property
     def has_ffmpeg_support(self):
-        # TODO
-        pass
+        """True if LAMMPS compiled with C++ ffmpeg support."""
+        return self._libc.lammps_config_has_ffmpeg_support != 0
 
     @property
     def installed_packages(self):
-        # TODO
-        pass
-
+        """List of additional LAMMPS packages."""
+        n_packages = self._libc.lammps_config_package_count()
+        buff_size = 100
+        str_buff = ct.create_string_buffer(buff_size)
+        installed_packages = []
+        for i in range(n_packages):
+            self._libc.lammps_config_package_name(i, str_buff, buff_size)
+            installed_packages.append(str_buff.value.decode())
+        return installed_packages
 
     def close(self):
         """Close the LAMMPS instance."""
@@ -78,7 +84,7 @@ class LAMMPS(object):
         """Returns the LAMMPS version string."""
         return self._libc.lammps_version(self._lammps_ptr)
 
-    # file has special meaning in Python so rename to run_file
+    # `file` has special meaning in Python so rename to `run_file`
     def run_file(self, path):
         """Runs an entire LAMMPS input script.
         
@@ -87,6 +93,7 @@ class LAMMPS(object):
         """
         if type(path) is not str:
             raise TypeError("`path` must be of type str")
+
         encoding = path.encode()
         self._libc.lammps_file(self._lammps_ptr, encoding)
 
@@ -98,6 +105,7 @@ class LAMMPS(object):
         """
         if type(cmd) is not str:
             raise TypeError("`cmd` must be of type str")
+
         encoding = cmd.encode()
         self._libc.lammps_command(self._lammps_ptr, encoding)
         # error handling
@@ -117,6 +125,7 @@ class LAMMPS(object):
         """
         if type(cmd_list) is not list:
             raise TypeError("`cmd_list` must be of type list")
+
         cmds = [cmd.encode() for cmd in cmd_list if type(cmd) is str]
         args = (ct.c_char_p * len(cmd_list))(*cmds)  # not really sure what this line does
         self._libc.lammps_commands_list(self._lammps_ptr, len(cmd_list), args)
@@ -129,6 +138,7 @@ class LAMMPS(object):
         """
         if type(multi_cmd) is not str:
             raise TypeError("`multi_cmd` must be of type str")
+
         encoding = multi_cmd.encode()
         self._libc.lammps_comand_string(self._lammps_ptr, ct.c_char_p(encoding))
 
@@ -143,6 +153,7 @@ class LAMMPS(object):
         """
         if type(name) is not str:
             raise TypeError("`name` must be of type str")
+
         encoding = name.encode()
         self._libc.lammps_extract_setting.restype = ct.c_int
         return int(self._libc.lammps_extract_settings(self._lammps_ptr, encoding))
@@ -159,6 +170,7 @@ class LAMMPS(object):
             raise TypeError("`name` must be of type str")
         if type(t) is not int:
             raise TypeError("`t` must be of type int")
+
         encoding = name.encode()
         if t == 0:
             self._libc.lammps_extract_global.restype = ct.POINTER(ct.c_int)
@@ -169,9 +181,8 @@ class LAMMPS(object):
         ptr = self._libc.lammps_extract_global(self._lammps_ptr, encoding)
         return ptr[0]  # not sure why only the zeroeth is returned
 
-    # this seems like it would need a name of the box to extract
     def extract_box(self):
-        """Extract a LAMMPS box.
+        """Extract the LAMMPS simulation box.
         
         Returns:
             dict
@@ -190,10 +201,10 @@ class LAMMPS(object):
         result = {
             "boxlo": boxlo[:3],
             "bokhi": boxhi[:3],
-            "xy": xy.value
-            "yz": yz.value
-            "xz": xz.value
-            "periodicity": periodicity[:3]
+            "xy": xy.value,
+            "yz": yz.value,
+            "xz": xz.value,
+            "periodicity": periodicity[:3],
             "box_change": box_change.value
         }
         return result
@@ -212,7 +223,8 @@ class LAMMPS(object):
         if type(name) is not str:
             raise TypeError("`name` must be of type str")
         if type(t) is not int:
-            raise TypeError("`t` must be of type int")    
+            raise TypeError("`t` must be of type int")
+
         encoding = name.encode()
         if t == 0:
             self._libc.lammps_extract_atom.restype = ct.POINTER(ct.c_int)
@@ -240,57 +252,321 @@ class LAMMPS(object):
         Returns:
             pointer
         """
-        # TODO
-        pass
+        if type(id_) is not str:
+            raise TypeError("`id_` must be of type str")
+        if type(style) is not int:
+            raise TypeError("`style` must be of type int")
+        if type(t) is not int:
+            raise TypeError("`t` must be of type int")
+        
+        if t == 0:
+            if style > 0:
+                raise ValueError("invalid `t` `style` combination: t={} style={}".format(t, style))
+            else:
+                self._libc.lammps_extract_compute.restype = ct.POINTER(ct.c_double)
+                ptr = self._libc.lammps_extract_compute(self._lammps_ptr, id_, style, t)
+                return ptr[0]
+        if t == 1:
+            self._libc.lammps_extract_compute.restype = POINTER(ct.c_double)
+            ptr = self._libc.lammps_extract_compute(self._lammps_ptr, id_, style, t)
+            return ptr
+        if t == 2:
+            if style == 0:
+                self._libc.lammps_extract_compute.restype = ct.POINTER(ct.c_int)
+                ptr = self._libc.lammps_extract_compute(self._lammps_ptr, id_, style, t)
+                return ptr[0]
+            else:
+                self._libc.lammps_extract_compute.restype = ct.POINTER(ct.POINTER(ct.c_double))
+                ptr = self._libc.lammps_extract_compute(self._lammps_ptr, id_, style, t)
+                return ptr
+        else:
+            raise ValueError("`t` must be 0, 1, or 2")
 
     def extract_fix(self, id_, style, t, i=0, j=0):
-        # TODO
-        pass
+        """Extract the result of a LAMMPS fix.
+        
+        Args:
+            id_ (str): ID of the fix.
+            style (int): Determines style of output
+            - <TODO>
+            t (int): Determines the type of output.
+            - <TODO>
+            i (optional) (int): <undocumented>
+            j (optional) (int): <undocumented>
+        
+        Returns:
+            pointer
+        """
+        if type(id_) is not str:
+            raise TypeError("`id_` must be of type str")
+        if type(style) is not int:
+            raise TypeError("`style` must be of type int")
+        if type(t) is not int:
+            raise TypeError("`t` must be of type int")
+        if type(i) is not int:
+            raise TypeError("`i` must be of type int")
+        if type(j) is not int:
+            raise TypeError("`j` must be of type int")
+
+        encoding = id_.encode()
+        if style == 0:
+            self._libc.lammps_extract_fix.restype = ct.POINTER(ct.c_double)
+            ptr = self._libc.lammps_extract_fix(self._lammps_ptr, encoding, style, t, i, j)
+            result = ptr[0]
+            self._libc.lammps_free(ptr)  # free in case of global datum
+            return result
+        elif (style == 1) or (style == 2):
+            if t == 1:
+                self._libc.lammps_extract_fix.restype = ct.POINTER(ct.c_double)
+            elif t == 2:
+                self._libc.lammps_extract_fix.restype = ct.POINTER(ct.POINTER(ct.c_double))
+            else:
+                raise ValueError("invalid `style` `t` combination: style={} t={}".format(style, t))
+            ptr = self._libc.lammps_extract_fix(self._lammps_ptr, encoding, style, t, i, j)
+            return ptr
+        else:
+            raise ValueError("`style` must be 0, 1, or 2")
+
 
     def extract_variable(self, name, group, t):
-        # TODO
-        pass
+        """Extract a LAMMPS variable.
+        
+        Args:
+            name (str): Name of the variable to extract.
+            group (str): Group to extract from.
+            t (int): Determines type of result.
+            - <TODO>
+        
+        Returns:
+            pointer
+        """
+        if type(name) is not str:
+            raise TypeError("`name` must be of type str")
+        if type(group) is not str:
+            raise TypeError("`group` must be of type str")
+        if type(t) is not int:
+            raise TypeError("`t` must be of type int")
+
+        name_encoding = name.encode()
+        group_encoding = group.encode()
+        if t == 0:
+            self._libc.lammps_extract_variable.restyle = ct.POINTER(ct.c_double)
+            ptr = self._libc.lammps_extract_variable(self._lammps_ptr, name_encoding, group_encoding)
+            result = ptr[0]
+            self._libc.lammps_free(ptr)
+            return result
+        elif t == 1:
+            self._libc.lammps_extract_global.restype = ct.POINTER(ct.c_int)
+            nlocalptr = self._libc.lammps_extract_global(self._lammps_ptr, "nlocal".encode())
+            nlocal = nlocalptr[0]
+            result = (ct.c_double*nlocal)()
+            self._libc.lammps_extract_variable.restype = ct.POINTER(ct.c_double)
+            ptr = self._libc.lammps_extract_variable(self._lammps_ptr, name_encoding, group_encoding)
+            for i in range(nlocal):
+                result[i] = ptr[i]
+            self._libc.lammps_free(ptr)
+            return result
+        else:
+            raise ValueError("`t` must be 0 or 1")
 
     def get_thermo(self, name):
-        # TODO
-        pass
+        """Returns current value of the thermo keyword.
+        
+        Args:
+            name (str): Thermo name to get.
+
+        Returns:
+            <undocumented>
+        """
+        if type(name) is not str:
+            raise TypeError("`name` must be of type str")
+        encoding = name.encode()
+        self._libc.lammps_get_thermo.restype = ct.c_double
+        return self._libc.lammps_get_thermo(self._lammps_ptr, encoding)
 
     def get_natoms(self):
-        # TODO
-        pass
+        """Returns the total number of atoms in the system."""
+        return self._libc.lammps_get_natoms(self._lammps_ptr)
 
     def set_variable(self, name, value):
-        # TODO
-        pass
+        """Sets a LAMMPS variable.
+        
+        Notes:
+            `value` is converted to str.
+
+        Args:
+            name (str): Name of the variable to set.
+            value (obj): Value to assign.
+        """
+        name_encoding = name.encode()
+        value_encoding = str(value).encode()
+        return self._libc.lamps_set_variable(self._lammps_ptr, name_encoding, value_encoding)
 
     def reset_box(self, boxlo, boxhi, xy, yz, xz):
-        # TODO
-        pass
+        """Resets simulation box size.
+        
+        Args:
+            boxlo (float): Lower bound.
+            boxhi (float): Upper bound.
+            xy (float): Tilt of x in y.
+            yz (float): Tilt of y in z.
+            xz (float): Tilt of x in z.
+        """
+        # TODO I'm lazy please just pass in floats
+        cboxlo = (3*ct.c_double)(*boxlo)
+        cboxhi = (3*ct.c_double)(*boxhi)
+        self._libc.lammps_reset_box(self._lammps_ptr, cboxlo, cboxhi, xy, yz, xz)
 
     def gather_atoms(self, name, t, count):
-        # TODO
-        pass
+        """Returns the properties of all atoms.
+        
+        Args:
+            name (str): <undocumented>
+            t (int): Determines return type.
+            - <TODO>
+            count (int): <undocumented>
+        
+        Returns:
+            <undocumented>
+        """
+        if type(name) is not str:
+            raise TypeError("`name` must be of type str")
+        if type(t) is not int:
+            raise TypeError("`t` must be of type int")
+        if type(count) is not int:
+            raise TypeError("`count` must be of type int")
+
+        encoding = name.encode()
+        natoms = self._libc.lammps_get_natoms(self._lammps_ptr)
+        if t == 0:
+            data = ((count*natoms)*ct.c_int)()
+            self._libc.lammps_gather_atoms(self._lammps_ptr, encoding, t, count, data)
+        elif t == 1:
+            data = ((count*natoms)*ct.c_double)()
+            self._libc.lammps_gather_atoms(self._lammps_ptr, encoding, t, count, data)
+        else:
+            raise ValueError("`t` must be 0 or 1")
+        return data
+        
 
     def gather_atoms_concat(self, name, t, count):
-        # TODO
-        pass
+        """Returns the properties of all atoms concatenated ???
+        
+        Args:
+            name (str): <undocumented>
+            t (int): Determines return type.
+            - <TODO>
+            count (int): <undocumented>
+        
+        Returns:
+            <undocumented>
+        """
+        if type(name) is not str:
+            raise TypeError("`name` must be of type str")
+        if type(t) is not int:
+            raise TypeError("`t` must be of type int")
+        if type(count) is not int:
+            raise TypeError("`count` must be of type int")
+
+        encoding = name.encode()
+        natoms = self._libc.lammps_get_natoms(self._lammps_ptr)
+        if t == 0:
+            data = ((count*natoms)*ct.c_int)()
+            self._libc.lammps_gather_atoms_concat(self._lammps_ptr, encoding, t, count, data)
+        elif t == 1:
+            data = ((count*natoms)*ct.c_double)()
+            self._libc.lammps_gather_atoms_concat(self._lammps_ptr, encoding, t, count, data)
+        else:
+            raise ValueError("`t` must be 0 or 1")
+        return data
 
     def gather_atoms_subset(self, name, t, count, ndata, ids):
-        # TODO
-        pass
+        """Returns the properties of a subset of all atoms.
+        
+        Args:
+            name (str): <undocumented>
+            t (int): Determines return type.
+            count (int): <undocumented>
+            ndata (int): <undocumented>
+            ids: <undocumented> presumably the atom ids one is interested in.
+        """
+        if type(name) is not str:
+            raise TypeError("`name` must be of type str")
+        if type(t) is not int:
+            raise TypeError("`t` must be of type int")
+        if type(count) is int:
+            raise TypeError("`count` must be of type int")
+        if type(ndata) is not int:
+            raise TypeError("`ndata` must be of type int")
+        # ids is probably a list???
+
+        encoding = name.encode()
+        if t == 0:
+            data = ((count*ndata)*ct.c_int)()
+            self._libc.lammps_gather_atoms_subset(self._lammps_ptr, encoding, t, count, ndata, ids, data)
+        elif t == 1:
+            data = ((count*ndata)*ct.c_double)()
+            self._libc.lammps_gather_atoms_subset(self._lammps_ptr, encoding, t, count, ndata, ids, data)
+        else:
+            raise ValueError("`t` must be 0 or 1")
+        return data
 
     def scatter_atoms(self, name, t, count, data):
-        # TODO
-        pass
+        """Scatter atom properties across processes.
+        
+        Args:
+            name (str): Name of the atomic property.
+            t (int): Determines return type.
+            - <TODO>
+            count (int): Number of per-atom values
+            - <TODO>
+            data (array): Data as collected by gather_atoms().
+        """
+        if type(name) is not str:
+            raise TypeError("`name` must be of type str")
+        if type(t) is not int:
+            raise TypeError("`t` must be of type int")
+        if type(count) is int:
+            raise TypeError("`count` must be of type int")
+        # not sure what type data is
+
+        encoding = name.encode()
+        self._libc.lammps_scatter_atoms(self._lammps_ptr, encoding, t, count, data)
 
     def scatter_atoms_subset(self, name, t, count, ndata, ids, data):
-        # TODO
-        pass
+        """Scatter a subset of atoms across processes."""
+        # TODO: docstring
+        encoding = name.encode()
+        self._libc.lammps_scatter_atoms_subset(self._lammps_ptr, encoding, t, count, ndata, ids, data)
 
     def create_atoms(self, n, id_, t, x, v, image=None, shrink_exceed=False):
-        # TODO
-        pass
+        """Mimic the LAMMPS `create_atoms` command.
+        
+        Args:
+            n (int): Global number of atoms.
+            id_ (optional) (int): ID of each atom.
+            t (int): Type of each atom.
+            x (array): Coordinates of each atom.
+            v (optional) (array): Velocity of each atom.
+            image ???
+            shrink_exceed ???
+        """
+        # TODO type checks
+        if id_:
+            id_lmp = (ct.c_int * n)()
+            id_lmp[:] = id_
+        else:
+            id_lmp = id_
 
+        if image:
+            image_lmp = (ct.c_int * n)()
+            image_lmp[:] = image
+        else:
+            image_lmp = image
+
+        type_lmp = (ct.c_int * n)()
+        type_lmp[:] = t
+        self._libc.lammps_create_atoms(self._lammps_ptr, n, id_lmp, type_lmp, x, v, image_lmp, shrink_exceed)
 
 
 if __name__ == "__main__":
