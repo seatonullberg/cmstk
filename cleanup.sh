3e4861e7d202cf2cc85c684c82e026f2c21c9d9c
seatonullberg <sullberg@ufl.edu>
2019-04-18 23:46:17 -0400

    added lammps linker script

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 find . | grep -E "(__pycache__|\.pyc)" | xargs rm -rf
 find . | grep -E "(.pytest_cache)" | xargs rm -rf
 find . | grep -E "(.cache)" | xargs rm -rf
 find . | grep -E "(.vscode)" | xargs rm -rf
+
+exit 0
---
+++ link-lammps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+start_dir="$(pwd)"
+dst=$1
+
+cd $dst
+git clone -b stable https://github.com/lammps/lammps.git
+cd lammps
+git pull
+cd src
+make purge
+make package-update
+make serial mode=shlib
+env_var="export LIBLAMMPS_SERIAL=${dst}lammps/src/liblammps_serial.so"
+
+if [[ "$SHELL" == "/bin/bash" ]]; then
+    echo $env_var >> ${HOME}.bashrc
+    source ${HOME}.bashrc
+elif [[ "$SHELL" == "/bin/zsh" ]]; then
+    echo $env_var >> ${HOME}.zshrc
+    source ${HOME}.zshrc
+else
+    echo "unsupported shell type"
+    echo "install aborted"
+    exit 1
+fi
+
+cd $start_dir
+
+echo "LAMMPS has been linked to cmstk"
+exit 0
