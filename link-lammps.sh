11e3b9902ec7b99dc1a137cfbbf0e46f05e6718e
seatonullberg <seatonullberg@gmail.com>
2019-04-20 20:29:26 -0400

    fixed linker script

--- link-lammps.sh
+++ link-lammps.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+set -e
+
 start_dir="$(pwd)"
 dst=$1
 
+# require a single command line argument (the destination to clone the lammps repo into)
 if [ -z "$dst" ]; then
     echo "no destination directory specified"
     echo "install aborted"
     exit 1
 fi
 
+# clone the lammps repo
 cd $dst
 git clone -b stable https://github.com/lammps/lammps.git
 cd lammps
@@ -17,14 +21,15 @@ cd src
 make purge
 make package-update
 make serial mode=shlib
-env_var="export LIBLAMMPS_SERIAL=${dst}lammps/src/liblammps_serial.so"
 
+# set the LIBLAMMPS_SERIAL environment variable cmstk expects
+env_var="export LIBLAMMPS_SERIAL=${dst}/lammps/src/liblammps_serial.so"
 if [ "$SHELL" == "/bin/bash" ]; then
-    echo $env_var >> ${HOME}.bashrc
-    source ${HOME}.bashrc
+    echo $env_var >> ${HOME}/.bashrc
+    source ${HOME}/.bashrc
 elif [ "$SHELL" == "/bin/zsh" ]; then
-    echo $env_var >> ${HOME}.zshrc
-    source ${HOME}.zshrc
+    echo $env_var >> ${HOME}/.zshrc
+    source ${HOME}/.zshrc
 else
     echo "unsupported shell type"
     echo "install aborted"
@@ -33,5 +38,6 @@ fi
 
 cd $start_dir
 
+echo ""
 echo "LAMMPS has been linked to cmstk"
 exit 0
