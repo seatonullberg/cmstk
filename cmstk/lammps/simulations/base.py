7be6bf651fb6483a907df80fe20aaa7eeb7560c4
seatonullberg <seatonullberg@gmail.com>
2019-04-28 12:52:03 -0400

    added stubs for the LAMMPS simulation base class

--- cmstk/lammps/simulations/base.py
+++ cmstk/lammps/simulations/base.py
@@ -3,38 +3,93 @@ from cmstk.lammps import LAMMPS
 
 class BaseLammpsSimulation(object):
     """Representation of a generalized LAMMPS simulation.
-    
+
     Args:
-        lammps_obj (LAMMPS): An instance of the LAMMPS interface object.
-        equal_vars (list of str): The names of equal-style variables to extract.
-        atom_vars (list of str): The names of atom-style variables to extract. 
+        lammps_obj (LAMMPS): An instance of the serial LAMMPS interface.
+    
+    Attributes:
+        lammps_obj (LAMMPS): An instance of the serial LAMMPS interface.
     """
 
-    def __init__(self, lammps_obj, equal_vars, atom_vars):
+    def __init__(self, lammps_obj):
         if type(lammps_obj) is not LAMMPS:
             raise TypeError("`lammps_obj` must be of type LAMMPS")
-        if type(equal_vars) is not list or type(atom_vars) is not list:
-            raise TypeError("`equal_vars` and `atom_vars` must be of type list")
-        for v in equal_vars + atom_vars:
-            if type(v) is not str:
-                raise TypeError("all members of `equal_vars` and `atom_vars` must be of type str")
+        self.lammps_obj = lammps_obj
+        self._quantities = {"atom":     [],
+                            "compute":  [],
+                            "fix":      [],
+                            "global":   [],
+                            "variable": []}
 
-        self._lammps_obj = lammps_obj
-        self._equal_vars = equal_vars
-        self._atom_vars = atom_vars
-        self._results = {}  # store simulation results in a dict to access through properties of the subclass
-
-    def simulate(self, cmd_str):
-        """Execute a `\n` separated string of commands and extract the desired variables.
-
-        Notes:
-            self._results is populated by calling this command but no value is returned.
-
+    def add_atom_quantity(self, name, t, shape):
+        """Add a per-atom quantity to extract from the simulation.
+        
         Args:
-            cmd_str (str): LAMMPS script in the format of a `\n` separated string.    
+            name (str): Name of the atomic quantity.
+            t (int): Determines format of the output.
+            - 0 for int vector, 1 for int array, 2 for double vector, 3 for double array
+            shape (tuple of ints): Desired shape of the result.
+            - (nrows, ncols) format.
         """
-        self._lammps_obj.commands_string(cmd_str)
-        for e_var in self._equal_vars:
-            self._results[e_var] = self._lammps_obj.extract_variable(e_var, 0)
-        for a_var in self._atom_vars:
-            self._results[a_var] = self._lammps_obj.extract_variable(a_var, 1)
+        pass
+
+    def add_compute_quantity(self, id_, style, t, shape=None):
+        """Adds a compute based quantity to extract from the simulation.
+        
+        Args:
+            id_ (str): ID of the compute quantity.
+            style (int): Determines the data group to extract from.
+            - 0 for global, 1 for per-atom, 2 for local.
+            t (int): Determines the type of the result.
+            - 0 for scalar, 1 for vector, 2 for array.
+            shape (optional) (tuple of ints): Desired shape of the output.
+            - required for vectors and arrays.
+        """ 
+        pass
+
+    def add_fix_quantity(self, id_, style, t, shape=None):
+        """Adds a fix based quantity to extract from the simulation.
+        
+        Args:
+            id_ (str): ID of the fix quantity.
+            style (int): Determine the data group to extract from.
+            - 0 for global, 1 for per-atom, 2 for local.
+            t (int): Determines the type of the result.
+            - 0 for scalar, 1 for vector, 2 for array.
+            shape (optional) (tuple of ints): Desired shape of the output.
+            - required for vectors and arrays
+        """
+        pass
+
+    def add_global_quantity(self, name, t):
+        """Extract a global quantity from the simulation.
+        
+        Args:
+            name (str): Name of the global quantity.
+            t (int): Determines the type of the result.
+            - 0 for int, 1 for double
+        """
+        pass
+
+    def add_variable_quantity(self, name, t, group=None):
+        """Extract a variable quantity from the simulation.
+        
+        Args:
+            name (str): Name of the variable.
+            t (int): Determines the type of the result.
+            - 0 for double, 1 for vector of doubles
+            group (optional) (str): Name of the group for atomic type variables.
+            - Required for atomic type varables.
+        """
+        pass
+
+    def simulate(self, filename=None, cmd_str=None):
+        """Exectue a LAMMPS simulation and extract the desired quantities.
+        
+        Args:
+            filename (optional) (str): File path to an input script to use.
+            cmd_str (optional) (str): Raw string of LAMMPS commands to use.
+
+            - one must be defined.
+        """
+        pass
