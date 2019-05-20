847ffae2fdfc74c030207fd0fe0f10e0dc2c7d2b
seatonullberg <sullberg@ufl.edu>
2019-05-20 16:32:29 -0400

    added more functionality to vasprun reader

--- cmstk/data/test_vasprun.py
+++ cmstk/data/test_vasprun.py
@@ -8,26 +8,58 @@ def test_init_vasprun_reader():
     vrr = VasprunReader(filename)
     assert vrr._data is not None
 
-def test_vasprun_reader_fermi_energy():
-    # tests fermi_energy access
-    filename = os.path.join("vasp", "Si_fcc.vasprun.xml")
-    vrr = VasprunReader(filename)
-    assert vrr.fermi_energy() == 6.31152884
-
 def test_vasprun_reader_dos():
     # tests dos access
     filename = os.path.join("vasp", "Si_fcc.vasprun.xml")
     vrr = VasprunReader(filename)
     dos = vrr.dos()
     assert dos.shape == (301, 3)
-    assert dos[-1][0] == 22.2839
+    assert dos[-1][0] == 37.8699
     assert dos[-1][1] == 0.0
-    assert dos[-1][2] == 12.0
+    assert dos[-1][2] == 16.0
 
 def test_vasprun_reader_eigenvalues():
     # tests eigenvalues access
     filename = os.path.join("vasp", "Si_fcc.vasprun.xml")
     vrr = VasprunReader(filename)
     eigenvalues = vrr.eigenvalues()
-    assert eigenvalues.shape == (1, 56, 6, 2)
-    assert eigenvalues[0][0][0][0] == -5.3966
+    assert eigenvalues.shape == (1, 286, 8, 2)
+    assert eigenvalues[0][0][0][0] == -4.3645
+
+def test_vasprun_reader_fermi_energy():
+    # tests fermi_energy access
+    filename = os.path.join("vasp", "Si_fcc.vasprun.xml")
+    vrr = VasprunReader(filename)
+    assert vrr.fermi_energy() == 9.85294972
+
+def test_vasprun_reader_kpoints():
+    # tests kpoints access
+    filename = os.path.join("vasp", "Si_fcc.vasprun.xml")
+    vrr = VasprunReader(filename)
+    kpts = vrr.kpoints()
+    assert kpts.shape == (286, 3)
+    assert kpts[-1][0] == -0.28571429
+    assert kpts[-1][1] == 0.47619048
+    assert kpts[-1][2] == 0.23809524
+
+def test_vasprun_reader_kpoints_path():
+    # tests kpoints_path access
+    filename = os.path.join("vasp", "Si_fcc.vasprun.xml")
+    vrr = VasprunReader(filename)
+    kpath = vrr.kpoints_path()
+    assert kpath.shape == (6, 3)
+    assert kpath[0][0] == 21
+    assert kpath[1][0] == 0.0
+    assert kpath[2][0] == 0.04761905
+    assert kpath[5][0] == 0.0
+
+
+def test_vasprun_reader_reciprocal_lattice():
+    # tests reciprocal_lattice access
+    filename = os.path.join("vasp", "Si_fcc.vasprun.xml")
+    vrr = VasprunReader(filename)
+    rcp_latt = vrr.reciprocal_lattice()
+    assert rcp_latt.shape == (3, 3)
+    assert rcp_latt[0][0] == 0.25641026
+    assert rcp_latt[0][1] == 0.25641026
+    assert rcp_latt[0][2] == -0.25641026
--- cmstk/data/vasprun.py
+++ cmstk/data/vasprun.py
@@ -15,19 +15,20 @@ class VasprunReader(BaseDataReader):
         super().__init__()
         self.read_xml(filename)
 
-    def dos(self, total=True):
+    def dos(self, is_total=True):
         """Returns an array of the density of states at all energy levels.
         
         Args:
-            total (optional) (bool): Determines whether `total` or `partial` is extracted.
+            is_total (optional) (bool): Determines whether `total` or `partial` DOS is extracted.
         
         Returns:
             numpy.ndarray
             - col 0: energy
             -col 1, 2: density
         """
+        ts.is_type((is_total, bool, "is_total"))
         dos_section = self._data.find("calculation").find("dos")
-        if total:
+        if is_total:
             arr = dos_section.find("total")
         else:
             arr = dos_section.find("partial")
@@ -68,3 +69,50 @@ class VasprunReader(BaseDataReader):
         dos_section = self._data.find("calculation").find("dos")
         e_fermi = float(dos_section.find("i").text)
         return e_fermi
+
+    def kpoints(self):
+        """Returns an array of the k-points.
+        
+        Returns:
+            numpy.ndarray
+        """
+        kpts_raw = self._data.find('kpoints').findall('varray')[0]
+        n_kpts = len(kpts_raw)
+        kpts = np.zeros(shape=(n_kpts, 3))
+        for i in range(n_kpts):
+            for j in range(3):
+                kpts[i, j] = float(kpts_raw[i].text.split()[j])
+        return kpts
+
+    def kpoints_path(self):
+        """Returns the k-points generation path.
+        
+        Returns:
+            numpy.ndarray
+        """
+        generation = self._data.find('kpoints').find('generation').findall('v')
+        kpath = np.zeros([len(generation), 3])
+        for i in range(len(generation)):
+            for j in range(3):
+                kpath[i, j] = float(generation[i].text.split()[j])
+        return kpath
+
+    def reciprocal_lattice(self, is_initial=True):
+        """Returns lattice vectors of the reciprocal lattice.
+        
+        Args:
+            is_initial (optional) (bool): Determines if the initial or final lattice vectors are returned.
+
+        Returns:
+            numpy.ndarray
+        """
+        ts.is_type((is_initial, bool, "is_initial"))
+        rcp_latt = np.zeros(shape=(3, 3))
+        if is_initial:
+            rcp_raw = self._data.findall('structure')[0].find('crystal').findall('varray')[1]
+        else:
+            rcp_raw = self._data.findall('structure')[1].find('crystal').findall('varray')[1]
+        for i in range(3):
+            for j in range(3):
+                rcp_latt[i, j] = float(rcp_raw[i].text.split()[j])
+        return rcp_latt
