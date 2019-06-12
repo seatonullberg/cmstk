2a3c9f5a7bbefef9d328b48635ba56d5925a9474
seatonullberg <seatonullberg@gmail.com>
2019-06-12 17:17:27 -0400

    cleaned up naming conventions in vasp

--- cmstk/vasp/poscar_test.py
+++ cmstk/vasp/poscar_test.py
@@ -12,10 +12,10 @@ def test_poscar_file():
 
     poscar_writer = PoscarFile("test.poscar")
     poscar_writer.comment = poscar.comment
-    poscar_writer.lattice_parameter = poscar.lattice_parameter
+    poscar_writer.lattice_constant = poscar.lattice_constant
     poscar_writer.total_volume = poscar.total_volume
     poscar_writer.lattice_vectors = poscar.lattice_vectors
-    poscar_writer.n_atoms_per_species = poscar.n_atoms_per_species
+    poscar_writer.n_atoms_per_symbol = poscar.n_atoms_per_symbol
     poscar_writer.selective_dynamics = poscar.selective_dynamics
     poscar_writer.coordinate_system = poscar.coordinate_system
     poscar_writer.positions = poscar.positions
@@ -25,11 +25,11 @@ def test_poscar_file():
     poscar_reader = PoscarFile("test.poscar")
     poscar_reader.read()
     assert poscar_reader.comment == poscar.comment
-    assert poscar_reader.lattice_parameter == poscar.lattice_parameter
+    assert poscar_reader.lattice_constant == poscar.lattice_constant
     assert poscar_reader.total_volume == poscar.total_volume
     assert np.array_equal(poscar_reader.lattice_vectors,
                           poscar.lattice_vectors)
-    assert poscar_reader.n_atoms_per_species == poscar.n_atoms_per_species
+    assert poscar_reader.n_atoms_per_symbol == poscar.n_atoms_per_symbol
     assert poscar_reader.selective_dynamics == poscar.selective_dynamics
     assert poscar_reader.coordinate_system == poscar.coordinate_system
     assert np.array_equal(poscar_reader.positions,
