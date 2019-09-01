522cc75ca62ba75843cd0d44f03e4eb7c8554cd0
Seaton Ullberg <seatonullberg@gmail.com>
2019-08-31 22:00:08 -0400

    added converge_kpoints

--- cmstk/workflows/vasp/convergence.py
+++ cmstk/workflows/vasp/convergence.py
@@ -6,7 +6,7 @@ from cmstk.vasp.potcar import PotcarFile
 from cmstk.workflows.vasp.base import VaspCalculation
 import copy
 import os
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Tuple
 
 
 def converge_encut(calculation: VaspCalculation,
@@ -19,7 +19,11 @@ def converge_encut(calculation: VaspCalculation,
         encut_values: The ENCUT values to test.
         working_directory: Path to the working directory.
     """
-    d = {encut: copy.deepcopy(calculation) for encut in encut_values}
+    # This needs work
+    d = {
+        encut: copy.deepcopy(calculation) 
+        for encut in encut_values
+    }
     # setup directories
     for encut, calc in d.items():
         dirname = "{}eV".format(encut)
@@ -44,55 +48,37 @@ def converge_encut(calculation: VaspCalculation,
         cmd = "{} {}".format(calc.submission_script.exec_cmd, path)
         os.system(cmd)
 
-# TODO:
-if False:
-    def converge_kpoints(kpoint_sizes: List[List[int]],
-                        incar: IncarFile,
-                        kpoints: KpointsFile,
-                        poscar: PoscarFile,
-                        potcar: PotcarFile,
-                        submission_script: Any,
-                        working_directory: Optional[str] = None) -> None:
-        """Runs a KPOINTS convergence calculation.
-        
-        Args:
-            kpoint_sizes: The mesh sizes to test.
-            incar: The INCAR file to use.
-            kpoints: The KPOINTS file to use.
-            - Any value of mesh_size will be overwritten
-            poscar: The POSCAR file to use.
-            potcar: The POTCAR file to use.
-            submission_script: The job submission script to use.
-            working_directory: The directory in which calculations are setup.
-        """
-        # setup directories
-        if working_directory is None:
-            working_directory = os.getcwd()
-        calculation_directories = []
-        for size in kpoint_sizes:
-            dirname = "{}x{}x{}".format(*size)
-            path = os.path.join(working_directory, dirname)
-            if not os.path.exists(path):
-                os.makedirs(path)
-            calculation_directories.append(path)
-        # iterate through each directory
-        for i, calc_dir in enumerate(calculation_directories):
-            # process KPOINTS
-            mesh_size = kpoint_sizes[i]
-            kpoints.mesh_size = mesh_size
-            _submit_vasp_calculation(incar, kpoints, poscar, potcar, 
-                                    submission_script, calc_dir)
 
-
-    def _submit_vasp_calculation(incar: IncarFile, kpoints: KpointsFile,
-                                poscar: PoscarFile, potcar: PotcarFile,
-                                script: Any, calc_dir: str) -> None: # NEED ABSTRACT SubmissionScript
-        incar.write(os.path.join(calc_dir, "INCAR"))
-        kpoints.write(os.path.join(calc_dir, "KPOINTS"))
-        poscar.write(os.path.join(calc_dir, "POSCAR"))
-        potcar.write(os.path.join(calc_dir, "POTCAR"))
-        path = os.path.join(calc_dir, "runjob.sh")
-        script.write(os.path.join(calc_dir, path))
+def converge_kpoints(calculation: VaspCalculation,
+                     kpoint_sizes: List[Tuple[int, int, int]],
+                     working_directory: str) -> None:
+    """Runs a KPOINTS convergence calculation.
+    
+    Args:
+        calculation: The VaspCalculation object to clone and converge.
+        kpoint_sizes: The KPOINTS mesh sizes to test.
+        working_directory: Path to the working directory.
+    """
+    d = {
+        kpoint_size: copy.deepcopy(calculation) 
+        for kpoint_size in kpoint_sizes
+    }
+    # setup directories
+    for kpoint_size, calc in d.items():
+        dirname = "{}x{}x{}".format(*kpoint_size)
+        calc_dir = os.path.join(working_directory, dirname)
+        if not os.path.exists(calc_dir):
+            os.makedirs(calc_dir)
+        calc.calculation_directory = calc_dir
+        if calc.kpoints is None:
+            err = "Missing required input file (KPOINTS)."
+            raise ValueError(err)
+        calc.kpoints.mesh_size = kpoint_size
+        calc.write()
         os.chdir(calc_dir)
-        cmd = "{} {}".format(script.exec_cmd, path)
+        if calc.submission_script is None:
+            err = "Missing required input file (submission script)."
+            raise ValueError(err)
+        path = os.path.join(calc_dir, "runjob.sh")
+        cmd = "{} {}".format(calc.submission_script.exec_cmd, path)
         os.system(cmd)
