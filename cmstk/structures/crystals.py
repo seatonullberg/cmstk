1e3c9a261079369cb7cac8b4fdf23047caa5fd19
seatonullberg <sullberg@ufl.edu>
2019-10-02 12:04:46 -0400

    fixed lattice directions in 110

--- cmstk/structures/crystals.py
+++ cmstk/structures/crystals.py
@@ -17,7 +17,7 @@ class Lattice(AtomCollection):
         coordinate_matrix: Parameters and angles combined into a 3x3 matrix.
         tolerance: The radius in which to check for atoms on add or remove.
         
-        Inhereted:
+        Inherited:
             charges: Electronic charge of each atom.
             magnetic_moments: Magnetic moment of each atom.
             n_atoms: Number of atoms in the collection.
@@ -44,7 +44,12 @@ class Lattice(AtomCollection):
             yield position / parameters
 
     def surface_area(self, scaling_factor: float = 1.0) -> float:
-        """Returns the surface area of the lattice."""
+        """Returns the surface area of the lattice.
+        
+        Args:
+            scaling_factor: Multiplicative scaling factor applied to both the x 
+            and y vectors.
+        """
         x = self.coordinate_matrix[0] * scaling_factor
         y = self.coordinate_matrix[1] * scaling_factor
         magx = np.linalg.norm(x)
