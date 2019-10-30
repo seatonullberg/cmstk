b8ef6e77c57286366042f10281987a234c4a9a5b
Seaton Ullberg <seatonullberg@gmail.com>
2019-10-29 22:42:57 -0400

    fixed annoying XML type checking

--- cmstk/vasp/vasprun.py
+++ cmstk/vasp/vasprun.py
@@ -1,9 +1,8 @@
-# type: ignore
-# This file ignores all type hints because handling them while
-# parsing XML is a massive hassle
 import numpy as np
 from typing import Optional
 import xml.etree.ElementTree as ET
+
+_bad_xml_err = "Unable to find or parse section `{}`."
 
 
 class VasprunFile(object):
@@ -19,6 +18,10 @@ class VasprunFile(object):
 
     Attributes:
         filepath: Filepath to a vasprun.xml file.
+        density_of_states: Total density of states.
+        eigenvalues: Projected or actual eigenvalues. 
+        eigenvectors: Electron eigenvectors projected onto atomic orbitals.
+        fermi_energy: The calculated Fermi Energy.
     """
 
     def __init__(self, filepath: Optional[str] = None) -> None:
@@ -27,25 +30,57 @@ class VasprunFile(object):
         self.filepath = filepath
         self._root = ET.parse(self.filepath).getroot()
 
+    @property
     def density_of_states(self) -> np.ndarray:
-        """Returns the total density of states."""
-        flag = "total"
-        dos_input = self._root.find("calculation").find("dos").find(
-            flag)[0][-1][-1]
+        dos_input = self._root.find("calculation")
+        if dos_input is None:
+            err = _bad_xml_err.format("calculation")
+            raise ValueError(err)
+        dos_input = dos_input.find("dos")
+        if dos_input is None:
+            err = _bad_xml_err.format("dos")
+            raise ValueError(err)
+        dos_input = dos_input.find("total")
+        if dos_input is None:
+            err = _bad_xml_err.format("total")
+            raise ValueError(err)
+        dos_input = dos_input[0][-1][-1]
         dos = np.zeros((len(dos_input), 3))
         for i, row in enumerate(dos_input):
-            dos[i, 0] = float(row.text.split()[0])  # energy
-            dos[i, 1] = float(row.text.split()[1])
-            dos[i, 2] = float(row.text.split()[2])
+            text = row.text
+            if text is None:
+                err = _bad_xml_err.format("total")
+                raise ValueError(err)
+            dos[i, 0] = float(text.split()[0])  # energy
+            dos[i, 1] = float(text.split()[1])
+            dos[i, 2] = float(text.split()[2])
         return dos
 
+    @property
     def eigenvalues(self) -> np.ndarray:
+        data = self._root.find("calculation")
+        if data is None:
+            err = _bad_xml_err.format("calculation")
+            raise ValueError(err)
+        is_projected = True
         try:
-            data = self._root.find("calculation").find("projected").find(
-                "eigenvalues").find("array")[-1]
+            _ = data.find("projected")
         except AttributeError:
-            data = self._root.find("calculation").find("eigenvalues").find(
-                "array")[-1]
+            is_projected = False
+        if is_projected:
+            data = data.find("projected")
+            if data is None:
+                err = _bad_xml_err.format("projected")
+                raise ValueError(err)
+        data = data.find("eigenvalues")
+        if data is None:
+            err = _bad_xml_err.format("eigenvalues")
+            raise ValueError(err)
+        data = data.find("array")
+        if data is None:
+            err = _bad_xml_err.format("array")
+            raise ValueError(err)
+        data = data[-1]
         n_spins = len(data)
         n_kpoints = len(data[0])
         n_bands = len(data[0][0])
@@ -54,19 +89,37 @@ class VasprunFile(object):
             for j in range(n_kpoints):
                 for k in range(n_bands):
                     for l in range(2):
-                        eigenvalues[i, j, k, l] = float(
-                            data[i][j][k].text.split()[l])
+                        text = data[i][j][k].text
+                        if text is None:
+                            err = _bad_xml_err.format("array")
+                            raise ValueError(err)
+                        eigenvalues[i, j, k, l] = float(text.split()[l])
         return eigenvalues
 
+    @property
     def eigenvectors(self) -> np.ndarray:
-        """Returns the electron eigenvectors projected onto atomic orbitals."""
-        projection = self._root.find("calculation").find("projected").find(
-            "array")[-1]
+        projection = self._root.find("calculation")
+        if projection is None:
+            err = _bad_xml_err.format("calculation")
+            raise ValueError(err)
+        projection = projection.find("projected")
+        if projection is None:
+            err = _bad_xml_err.format("projected")
+            raise ValueError(err)
+        projection = projection.find("array")
+        if projection is None:
+            err = _bad_xml_err.format("array")
+            raise ValueError(err)
+        projection = projection[-1]
         n_spins = len(projection)
         n_kpoints = len(projection[0])
         n_bands = len(projection[0][0])
         n_ions = len(projection[0][0][0])
-        n_orbitals = len(projection[0][0][0][0].text.split())
+        text = projection[0][0][0][0].text
+        if text is None:
+            err = _bad_xml_err.format("array")
+            raise ValueError(err)
+        n_orbitals = len(text.split())
         eigenvectors = np.zeros(
             (n_spins, n_kpoints, n_bands, n_ions, n_orbitals))
         for i in range(n_spins):
@@ -74,26 +127,29 @@ class VasprunFile(object):
                 for k in range(n_bands):
                     for l in range(n_ions):
                         for m in range(n_orbitals):
-                            eigenvectors[i, j, k, l, m] = float(
-                                projection[i][j][k][l].text.split()[m])
+                            text = projection[i][j][k][l].text
+                            if text is None:
+                                err = _bad_xml_err.format("array")
+                                raise ValueError(err)
+                            eigenvectors[i, j, k, l, m] = float(text.split()[m])
         return eigenvectors
 
+    @property
     def fermi_energy(self) -> float:
-        """Returns the calculated fermi energy."""
-        return float(self._root.find("calculation").find("dos").find("i").text)
-
-    def reciprocal_lattice(self, initial: bool = True) -> np.ndarray:
-        """Returns the initial or final reciprocal lattice vectors.
-        
-        Args:
-            initial: Bool flag to determine initial or final lattice vectors.
-        """
-        index = (0 if initial else -1)
-        lattice = np.zeros((3, 3))
-        lattice_entry = self._root.findall("structure")[index].find(
-            "crystal").findall("varray")[1]
-        for i in range(3):
-            temp = lattice_entry[i].text.split()
-            for j in range(3):
-                lattice[i, j] = float(temp[j])
-        return lattice
+        fermi = self._root.find("calculation")
+        if fermi is None:
+            err = _bad_xml_err.format("calculation")
+            raise ValueError(err)
+        fermi = fermi.find("dos")
+        if fermi is None:
+            err = _bad_xml_err.format("dos")
+            raise ValueError(err)
+        fermi = fermi.find("i")
+        if fermi is None:
+            err = _bad_xml_err.format("i")
+            raise ValueError(err)
+        text = fermi.text
+        if text is None:
+            err = _bad_xml_err.format("i")
+            raise ValueError(err)
+        return float(text)
