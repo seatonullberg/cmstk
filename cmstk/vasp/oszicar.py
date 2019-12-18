510ed8ff27de143ac698ae96a99acfbd118413b4
seatonullberg <sullberg@ufl.edu>
2019-12-18 15:09:28 -0500

    updated OSZICAR file

--- cmstk/vasp/oszicar.py
+++ cmstk/vasp/oszicar.py
@@ -1,21 +1,18 @@
-from typing import Optional, Sequence
+from cmstk.filetypes import TextFile
+from typing import Optional, List
 
 
-class OszicarFile(object):
+class OszicarFile(TextFile):
     """File wrapper from a VASP OSZICAR file.
 
     Notes:
         This is a read-only file wrapper.
-        This wrapper is not robust to a wide range of IBRION values.
-        TODO:
-        - Future updates must check keyword rather than order.
 
     Args:
         filepath: Filepath to an OSZICAR file.
      
      Attributes:
         filepath: Filepath to an OSZICAR file.
-        delta_energy: Change in total energy at each ionic step.
         e0: Energy where sigma == 0 at each ionic step.
         magnetization: Magnetization at each ionic step.
         total_free_energy: Total free energy at each ionic step.
@@ -24,36 +21,41 @@ class OszicarFile(object):
     def __init__(self, filepath: Optional[str] = None) -> None:
         if filepath is None:
             filepath = "OSZICAR"
-        self.filepath = filepath
-        self.total_free_energy: Sequence[float] = ()
-        self.e0: Sequence[float] = ()
-        self.delta_energy: Sequence[float] = ()
-        self.magnetization: Sequence[float] = ()
+        self._total_free_energy: Optional[List[float]] = None
+        self._e0: Optional[List[float]] = None
+        self._magnetization: Optional[List[float]] = None
+        super().__init__(filepath)
 
-    def read(self, path: Optional[str] = None) -> None:
-        """Reads an OSZICAR file.
-        
-        Args:
-            path: The filepath to read from.
-        """
-        if path is None:
-            path = self.filepath
-        with open(path, "r") as f:
-            lines = f.readlines()
-        total_free_energy_values = []
-        e0_values = []
-        delta_energy_values = []
-        magnetization_values = []
-        # only parse ionic step lines
-        # - they always start with an int
-        lines = [l for l in lines if l.strip()[0].isdigit()]
+    @property
+    def total_free_energy(self) -> List[float]:
+        if self._total_free_energy is None:
+            self._total_free_energy = self._get_values_for_symbol("F=")
+        return self._total_free_energy
+
+    @property
+    def e0(self) -> List[float]:
+        if self._e0 is None:
+            self._e0 = self._get_values_for_symbol("E0=")
+        return self._e0
+
+    @property
+    def magnetization(self) -> List[float]:
+        if self._magnetization is None:
+            self._magnetization = self._get_values_for_symbol("mag=")
+        return self._magnetization
+
+    def _get_ionic_lines(self) -> List[str]:
+        return [l for l in self.lines if l.strip()[0].isdigit()]
+
+    def _get_values_for_symbol(self, symbol: str) -> List[float]:
+        lines = self._get_ionic_lines()
+        values: List[float] = []
         for line in lines:
-            l = line.split()
-            total_free_energy_values.append(float(l[2]))
-            e0_values.append(float(l[4]))
-            delta_energy_values.append(float(l[7].replace("=", "")))
-            magnetization_values.append(float(l[9]))
-        self.total_free_energy = tuple(total_free_energy_values)
-        self.e0 = tuple(e0_values)
-        self.delta_energy = tuple(delta_energy_values)
-        self.magnetization = tuple(magnetization_values)
+            segments = [seg for seg in line.split() if len(seg.strip()) > 0]
+            for i, seg in enumerate(segments):
+                if seg == symbol:
+                    # append the next segment
+                    # because it is symbol's value
+                    values.append(float(segments[i+1]))
+                    break
+        return values
