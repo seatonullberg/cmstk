47c529b7f2205f9069bbf09725bce9ffe38eb6ee
Seaton Ullberg <seatonullberg@gmail.com>
2020-01-10 16:09:37 -0500

    unit testing for xyz file wrapper

--- cmstk/lammps/datafile.py
+++ cmstk/lammps/datafile.py
@@ -1,3 +1,4 @@
+# TODO
 from cmstk.filetypes import TextFile
 from cmstk.structure.simulation import SimulationCell
 import numpy as np
@@ -6,15 +7,15 @@ class DataFile(TextFile):
     def __init__(self, filepath, comment, simulation_cell):
         if filepath is None:
             filepath = "lammps.data"
-        
+
         if comment is None:
             comment = "This is a LAMMPS data file."
         self._comment = comment
         self._simulation_cell = simulation_cell
 
         super().__init__(filepath)
-    
-    @property 
+
+    @property
     def comment(self):
         if self._comment is None:
             self._comment = self.lines[0]
@@ -24,14 +25,3 @@ class DataFile(TextFile):
     def comment(self, value):
         self._comment = value
 
-    @property
-    def simulation_cell(self):
-        if self._simulation_cell is None:
-            self._simulation_cell = SimulationCell #?
-        
-
-        self._simulation_cell = simulation_cell
-        return self._simulation_cell
-
-    @simulation_cell.setter
-    def simulation_cell(self, atoms, bonds, angles, dihedrals, impropers):
--- cmstk/xyz.py
+++ cmstk/xyz.py
@@ -1,60 +1,71 @@
 from cmstk.filetypes import TextFile
-from cmstk.structure.atom import AtomCollection
+from cmstk.structure.atom import Atom, AtomCollection
 import numpy as np
+from typing import Optional
+
 
 class XyzFile(TextFile):
-    def __init__(self, filepath, comment, atom_collection):
-        if filepath is None:
-            filpath = "structure.xyz"
+    """File wrapper for a xyz formatted structure file.
 
-        if comment is None:
-            comment = "comment.xyz"
+    Notes:
+        File specification:
+        https://www.cgl.ucsf.edu/chimera/docs/UsersGuide/xyz.html
+
+    Args:
+        filepath: Path to a xyz file.
+        comment: The comment line.
+        atom_collection: The underlying collection of atoms.
+
+    Attributes:
+        filepath: Path to a xyz file.
+        comment: The comment line.
+        atom_collection: The underlying collection of atoms.
+    """
+
+    def __init__(self, filepath: Optional[str] = None, comment: Optional[str] = None, atom_collection: Optional[AtomCollection] = None) -> None:
+        if filepath is None:
+            filepath = "structure.xyz"
         self._comment = comment
         self._atom_collection = atom_collection
-
         super().__init__(filepath)
 
-        @property 
-        def comment(self):
-            if self._comment is None:
-             self._comment = self.lines[1]
+    @property
+    def comment(self) -> str:
+        if self._comment is None:
+            self._comment = self.lines[1]
         return self._comment
 
-        @comment.setter
-        def comment(self, value):
-            self._comment = value
-        
-        @property
-        def atom_collection:
-            if atom_collection is None:
-                Atoms = []
+    @comment.setter
+    def comment(self, value: str) -> None:
+        self._comment = value
 
-                for line in self.lines:
-                    parts = line.split()
-                    
-                    atom_type = parts[0]
-                    X = parts[1]
-                    Y = parts[2]
-                    Z = parts[3]
+    @property
+    def atom_collection(self) -> AtomCollection:
+        if self._atom_collection is None:
+            atoms = []
+            for line in self.lines[2:]:
+                parts = line.split()
+                symbol = parts[0]
+                position = np.array([float(parts[1]), float(parts[2]), float(parts[3])])
+                atoms.append(Atom(symbol=symbol, position=position))
+            self._atom_collection = AtomCollection(atoms=atoms)
+        return self._atom_collection
 
-                    position = np.array([X, Y, Z])
+    @atom_collection.setter
+    def atom_collection(self, value: AtomCollection) -> None:
+        self._atom_collection = value
 
-                    atom = Atom(atom_type = symbol, position = position)
-                    Atoms.append(atom)
-
-                
-                
-                
-
-
-                    
-
-                    
-                     
-            
-                
-
-
-        
-
-        
+    def write(self, path: Optional[str] = None) -> None:
+        if path is None:
+            path = self.filepath
+        n_atoms = self.atom_collection.n_atoms
+        if self._comment is None:
+            comment = "# painstakingly crafted by cmstk :)"
+        else:
+            comment = self._comment
+        with open(path, "w") as f:
+            f.write("{}\n".format(n_atoms))
+            f.write("{}\n".format(comment))
+            for a in self.atom_collection.atoms:
+                s = "{} {} {} {}\n".format(a.symbol, a.position[0], a.position[1], a.position[2])
+                f.write(s)
